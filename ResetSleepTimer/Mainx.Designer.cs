56ca095e6380c5838c7cae948a1d88c3abaaf0f1
pdone <617941447@qq.com>
2021-06-11 13:44:18 +0800

    添加项目文件。

---
+++ ResetSleepTimer/Mainx.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace ResetSleepTimer
+{
+    partial class Mainx
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.PauseStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AutobootStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Separator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.ExitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon.BalloonTipText = "本程序将通过重置系统休眠计时器，从而阻止系统自动休眠。";
+            this.notifyIcon.BalloonTipTitle = "重置休眠计时器";
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip;
+            this.notifyIcon.Text = "重置休眠计时器";
+            this.notifyIcon.Visible = true;
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.PauseStripMenuItem,
+            this.AutobootStripMenuItem,
+            this.Separator1,
+            this.ExitToolStripMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(141, 76);
+            // 
+            // PauseStripMenuItem
+            // 
+            this.PauseStripMenuItem.CheckOnClick = true;
+            this.PauseStripMenuItem.Name = "PauseStripMenuItem";
+            this.PauseStripMenuItem.Size = new System.Drawing.Size(140, 22);
+            this.PauseStripMenuItem.Text = "暂停(&P)";
+            // 
+            // AutobootStripMenuItem
+            // 
+            this.AutobootStripMenuItem.CheckOnClick = true;
+            this.AutobootStripMenuItem.Name = "AutobootStripMenuItem";
+            this.AutobootStripMenuItem.Size = new System.Drawing.Size(140, 22);
+            this.AutobootStripMenuItem.Text = "开机自启(&A)";
+            // 
+            // Separator1
+            // 
+            this.Separator1.Name = "Separator1";
+            this.Separator1.Size = new System.Drawing.Size(137, 6);
+            // 
+            // ExitToolStripMenuItem
+            // 
+            this.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem";
+            this.ExitToolStripMenuItem.Size = new System.Drawing.Size(140, 22);
+            this.ExitToolStripMenuItem.Text = "退出(&E)";
+            // 
+            // Mainx
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(0, 0);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Mainx";
+            this.Opacity = 0D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "重置休眠计时器";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Mainx_Load);
+            this.contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem ExitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem AutobootStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem PauseStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator Separator1;
+    }
+}
+
---
+++ ResetSleepTimer/Mainx.cs
@@ -0,0 +1,173 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ResetSleepTimer
+{
+    public partial class Mainx : Form
+    {
+        BackgroundWorker Worker;
+
+        /// <summary>
+        /// 重置间隔
+        /// </summary>
+        const int Interval = 1000 * 30;
+
+        /// <summary>
+        /// 启动项注册表路径
+        /// </summary>
+        const string RegistryPath = @"Software\Microsoft\Windows\CurrentVersion\Run";
+
+        public Mainx()
+        {
+            InitializeComponent();
+        }
+
+        private void Mainx_Load(object sender, EventArgs e)
+        {
+            string path = Application.ExecutablePath;
+
+            notifyIcon.Icon = Properties.Resources.logo;
+
+            notifyIcon.ShowBalloonTip(1000);
+            Worker = new BackgroundWorker()
+            {
+                WorkerSupportsCancellation = true
+            };
+
+            Worker.DoWork += (ss, ee) =>
+            {
+                while (true)
+                {
+                    if (Worker.CancellationPending)
+                    {
+                        ee.Cancel = true;
+                        return;
+                    }
+
+                    Thread.Sleep(Interval);
+                    SystemSleepManagement.ResetSleepTimer(true);
+                }
+            };
+            Worker.RunWorkerAsync();
+
+            PauseStripMenuItem.CheckedChanged += (ss, ee) =>
+            {
+                if (PauseStripMenuItem.Checked)
+                {
+                    if (Worker.IsBusy)
+                    {
+                        Worker.CancelAsync();
+                    }
+                }
+                else
+                {
+                    if (!Worker.IsBusy)
+                    {
+                        Worker.RunWorkerAsync();
+                    }
+                }
+            };
+
+            using (RegistryKey rk = Registry.LocalMachine)
+            {
+                using (RegistryKey rk2 = rk.OpenSubKey(RegistryPath))
+                {
+                    var path2 = rk2.GetValue("ResetSleepTimer") as string;
+                    if (path == path2)
+                    {
+                        AutobootStripMenuItem.Checked = true;
+                    }
+                }
+            }
+
+
+            AutobootStripMenuItem.CheckedChanged += (ss, ee) =>
+            {
+                if (AutobootStripMenuItem.Checked)
+                {
+                    using (RegistryKey rk = Registry.LocalMachine)
+                    {
+                        using (RegistryKey rk2 = rk.CreateSubKey(RegistryPath))
+                        {
+                            rk2.SetValue("ResetSleepTimer", path);
+                        }
+                    }
+                }
+                else
+                {
+                    using (RegistryKey rk = Registry.LocalMachine)
+                    {
+                        using (RegistryKey rk2 = rk.CreateSubKey(RegistryPath))
+                        {
+                            rk2.SetValue("ResetSleepTimer", false);
+                        }
+                    }
+                }
+            };
+
+            ExitToolStripMenuItem.Click += (ss, ee) =>
+            {
+                Worker.CancelAsync();
+                Application.Exit();
+            };
+        }
+
+
+    }
+
+    class SystemSleepManagement
+    {
+        //定义API函数
+        [DllImport("kernel32.dll")]
+        static extern uint SetThreadExecutionState(ExecutionFlag flags);
+
+        [Flags]
+        enum ExecutionFlag : uint
+        {
+            System = 0x00000001,
+            Display = 0x00000002,
+            Continus = 0x80000000,
+        }
+
+        /// <summary>
+        /// 阻止系统休眠，直到线程结束恢复休眠策略
+        /// </summary>
+        /// <param name="includeDisplay">是否阻止关闭显示器</param>
+        public static void PreventSleep(bool includeDisplay = false)
+        {
+            if (includeDisplay)
+                SetThreadExecutionState(ExecutionFlag.System | ExecutionFlag.Display | ExecutionFlag.Continus);
+            else
+                SetThreadExecutionState(ExecutionFlag.System | ExecutionFlag.Continus);
+        }
+
+        /// <summary>
+        /// 恢复系统休眠策略
+        /// </summary>
+        public static void ResotreSleep()
+        {
+            SetThreadExecutionState(ExecutionFlag.Continus);
+        }
+
+        /// <summary>
+        /// 重置系统休眠计时器
+        /// </summary>
+        /// <param name="includeDisplay">是否阻止关闭显示器</param>
+        public static void ResetSleepTimer(bool includeDisplay = false)
+        {
+            if (includeDisplay)
+                SetThreadExecutionState(ExecutionFlag.System | ExecutionFlag.Display);
+            else
+                SetThreadExecutionState(ExecutionFlag.System);
+        }
+    }
+}
